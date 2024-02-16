1bdd06aded0eb09f3c7bde9afed87d9e97ec9bba
Mathias Buus <mathiasbuus@gmail.com>
2024-02-16 18:00:23 +0100

    fix in bare

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 'use strict'
 const { Writable } = require('streamx')
+const tty = require('tty')
 
-let tty = null
-try { tty = require('tty') } catch { tty = require('bare-tty') }
+const IS_BARE = !!global.Bare
 
-const Stdio = process.versions.bare
+const Stdio = IS_BARE
   ? class Stdio {
     drained = Writable.drained
     constructor () {
-      const { ReadStream, WriteStream } = this.tty
+      const { ReadStream, WriteStream } = tty
       this.in = new ReadStream(0)
       this.out = new WriteStream(1)
       this.err = new WriteStream(2)
@@ -22,7 +22,7 @@ const Stdio = process.versions.bare
 
     raw (rawMode) {
       this.rawMode = !!rawMode
-      return this.in.setMode(this.rawMode ? this.tty.constants.MODE_RAW : this.tty.constants.MODE_NORMAL)
+      return this.in.setMode(this.rawMode ? tty.constants.MODE_RAW : tty.constants.MODE_NORMAL)
     }
   }
   : class Stdio {
